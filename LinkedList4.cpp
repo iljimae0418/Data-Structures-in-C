8a51499c2eda39e9c3f4cfc31afa348b3bae84c6
Minsuk Kim (Luke) <minsukkim9@gmail.com>
2016-01-16 02:21:13 -0800

    Create LinkedList4.cpp

---
+++ LinkedList4.cpp
@@ -0,0 +1,22 @@
+#include <iostream> 
+#include <cstdlib> 
+using namespace std;
+
+// example code that shows how one might completely get rid of the linked list. 
+
+struct ListNode{
+	int data; 
+	struct ListNode *next; 
+}; 
+
+vodi DeleteLinkedList(struct ListNode **head){
+	struct ListNode *auxiliaryNode, *iterator;  
+	iterator = head; 
+	while (iterator){
+		auxiliaryNode = iterator->next; 
+		free(iterator); 
+		iterator = auxiliaryNode;  
+	}
+	free(head); 
+}
+
